56eb4c602e9cf85fbd77e8b5ec6726ef95be2f52
BadriVamsiKrish <www.1203badrivamsi@.com>
2023-05-02 08:21:13 +0530

    task 6 of rest web

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -2,10 +2,10 @@ import React,{Fragment} from 'react';
 import classes from './HeaderCartButton.module.css';
 import iconImage from '../../Assets/icons8-shopping-trolley-64.png'
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
   return (
     <Fragment>
-      <button className={classes.button}>
+      <button className={classes.button} onClick={props.onClick}>
         <span><img src={iconImage} alt='icon'/></span> 
         <span>
           Your cart
